f3bd83effbe8efdf1d194bf218e9e8b42e3f2a28
liraraujo <liracrist4@gmail.com>
2025-09-16 12:23:14 +0000

    aaaaaaaa

--- mindcreate/telas/login.js
+++ mindcreate/telas/login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, SafeAreaView } from 'react-native';
-import { loginUser } from "../services/authService";
+import { loginUser } from "../firebase/authfirebase";
 
 const LoginScreen = () => {
   const [email, setEmail] = useState('');
